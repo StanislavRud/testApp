bdbea1c33126724c7b13750801e83052c326a4d5
Stanislav <rudstanislav1990@gmail.com>
2021-06-29 22:57:15 +0300

    added redux-form

--- src/components/AppForm/index.jsx
+++ src/components/AppForm/index.jsx
@@ -1,13 +1,56 @@
-import React from "react";
-import { Field, reduxForm } from 'redux-form';
+import React, {useEffect} from "react";
+import {connect} from 'react-redux';
+import {initialize, Field, reduxForm, getFormValues} from 'redux-form';
 
-const AppForm = () => {
+let AppForm = ({formValues, initialize, error, handleSubmit, pristine, reset, submitting }) => {
+    useEffect(() => {
+        console.log(formValues);
+    }, [formValues]);
+
+    useEffect(() => {
+        initialize({});
+    }, []);
+
+    const renderField = ({ input, label, type, meta: { touched, error } }) => (
+        <div>
+            <label>{label}</label>
+            <div>
+                <input {...input} placeholder={label} type={type} />
+                {touched && error && <span>{error}</span>}
+            </div>
+        </div>
+    )
+
     return (
         <form action="">
             <label>Add form</label>
-
+            <Field
+                name="name"
+                component={renderField}
+                type="text"
+                label="Name"
+            />
+            <Field
+                name="text"
+                component={renderField}
+                type="text"
+                label="Text"
+            />
+            <button type="submit" disabled={submitting}>
+                Log In
+            </button>
         </form>
     );
 };
 
-export default reduxForm({ form: 'my-form' })(AppForm);
+AppForm = reduxForm({form: 'my-form'})(AppForm);
+
+const mapStateToProps = state => ({
+    formValues: getFormValues('my-form')(state),
+});
+
+const mapDispatchToProps = {
+    initialize,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppForm);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
-import { createStore, combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {reducer as formReducer} from 'redux-form'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const rootReducer = combineReducers({
-    // ...your other reducers here
-    // you have to pass formReducer under 'form' key,
-    // for custom keys look up the docs for 'getFormState'
-    form: formReducer
-});
+const rootReducer = combineReducers(
+    {
+        form: formReducer,
+    },
+    composeWithDevTools(applyMiddleware())
+);
 
-export const store = createStore(rootReducer);
+const store = createStore(rootReducer);
+
+export default store;
